6367d8181bf752c025567dc86e4c33ddbba97129
Ariel Salazar <arielsalazarraw@gmail.com>
2020-06-14 09:00:08 -0400

    Hard coded messages appear when page loads

--- portfolio/src/main/java/com/google/sps/servlets/MessagesServlet.java
+++ portfolio/src/main/java/com/google/sps/servlets/MessagesServlet.java
@@ -44,4 +44,9 @@ public class MessagesServlet extends HttpServlet {
         response.setContentType("application/json;");
         response.getWriter().println(json);
     }
+
+    @Override
+    public void doPost(HttpServletRequest request, HttpServletResponse response) throws IOException{
+        
+    }
 }
